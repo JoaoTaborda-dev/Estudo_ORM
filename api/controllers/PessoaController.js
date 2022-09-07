a15a3b9cf961013db85060fbf7b1c396357e3e1b
João Campos <joaog.taborda.campos@gmail.com>
2022-09-06 23:56:15 -0300

    Buscando somente pessoas ativas no get de pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -4,7 +4,9 @@ class PessoaController {
   // Busca de Registro
   static async pegaTodasAsPessoas(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoas.findAll()
+      const todasAsPessoas = await database.Pessoas.findAll({
+        where: { ativo: Number(1) }
+      })
       return res.status(200).json(todasAsPessoas)
     } catch (error) {
       return res.status(500).json(error.message)
